accd21c262c2b380967d07bf89422319cfde2a8a
assassini <assassinig@gmail.com>
2014-02-16 17:47:33 +0200

    Add an asset postprocessor for VMFs

---
+++ Assets/VMFPostprocessor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Linq;
+
+public class VMFPostprocessor : AssetPostprocessor
+{
+	private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromPath)
+	{
+		importedAssets = importedAssets.Where(x => Path.GetExtension(x) == ".vmf").ToArray();
+		deletedAssets = deletedAssets.Where(x => Path.GetExtension(x) == ".vmf").ToArray();
+		movedAssets = movedAssets.Where(x => Path.GetExtension(x) == ".vmf").ToArray();
+		movedFromPath = movedFromPath.Where(x => Path.GetExtension(x) == ".vmf").ToArray();
+
+		foreach (string asset in importedAssets)
+		{
+			Debug.Log("VMF imported: " + asset);
+		}
+		
+		foreach (string asset in deletedAssets)
+		{
+			Debug.Log("VMF deleted: " + asset);
+		}
+
+		for (int i = 0; i < movedAssets.Length; i++)
+		{
+			Debug.Log("VMF moved from " + movedFromPath[i] + " to " + movedAssets[i]);
+		}
+	}
+}
