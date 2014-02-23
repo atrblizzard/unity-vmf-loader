ac027f666d245fe20c43ca2cc4de7a0999c142b5
Paavo Huhtala <paavo.huhtala@gmail.com>
2014-02-23 16:37:25 +0200

    Parse entity properties

--- Assets/Nodes/Entity.cs
+++ Assets/Nodes/Entity.cs
@@ -1,16 +1,70 @@
-using UnityEngine;
-using System;
-using System.IO;
-using System.Linq;
-using System.Collections;
-using System.Collections.ObjectModel;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
+using System.Linq;
+using UnityEngine;
 
 namespace UnityVMFLoader.Nodes
 {
 	public class Entity : Node
 	{
+		public string ClassName;
 
+		public Quaternion Angles;
+		public Vector3 Origin;
+
+		private readonly Dictionary<string, string> properties;  
+
+		public Entity()
+		{
+			properties = new Dictionary<string, string>();
+		}
+
+		public override void Parse(string key, string value)
+		{
+			base.Parse(key, value);
+
+			switch(key)
+			{
+				case "classname":
+
+					ClassName = value;
+
+					break;
+
+				case "angles":
+
+					var axis = value.Split(' ').Select(v => float.Parse(v)).ToArray();
+
+					Angles = Quaternion.Euler(axis[0], axis[2], axis[1]);
+
+					break;
+
+				case "origin":
+
+					var origin = value.Split(' ').Select(v => float.Parse(v)).ToArray();
+
+					Origin = new Vector3(origin[0], origin[2], origin[1]);
+
+					break;
+				
+				default:
+
+					properties[key] = value;
+
+					break;
+			}
+		}
+
+		public string this[string key]
+		{
+			get
+			{
+				return properties[key];
+			}
+
+			set
+			{
+				properties[key] = value;
+			}
+		}
 	}
 }
